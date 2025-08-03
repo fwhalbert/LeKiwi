cf8e5f5b79803d69ae6e015eaa5c5d9e840e982c
albert <feiwanghua@gmail.com>
2025-08-03 16:32:21 +0800

    first success run ros2 control

---
+++ launch/lekiwi.launch.py
@@ -0,0 +1,103 @@
+# Copyright 2025 Albert Robotics
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from launch import LaunchDescription
+from launch.substitutions import (
+    Command,
+    PathJoinSubstitution,
+)
+
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    control_node = Node(
+        package="controller_manager",
+        executable="ros2_control_node",
+        parameters=[
+            PathJoinSubstitution(
+                [
+                    FindPackageShare("lekiwi"),
+                    "config",
+                    "ros2_controllers.yaml",
+                ]
+            )
+        ],
+        remappings=[
+            ("~/robot_description", "/robot_description"),
+            ("~/cmd_vel", "/cmd_vel"),
+            ("~/odom", "/odom"),
+        ],
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[
+            {
+                "robot_description": Command(
+                    [
+                        "xacro ",
+                        PathJoinSubstitution(
+                            [
+                                FindPackageShare("lekiwi"),
+                                "config",
+                                "lekiwi.urdf.xacro",
+                            ]
+                        ),
+                    ]
+                )
+            }
+        ],
+    )
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=[
+            "-d",
+            PathJoinSubstitution([FindPackageShare("lekiwi"), "rviz", "lekiwi.rviz"]),
+        ],
+    )
+
+    joint_state_broadcaster_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=[
+            "joint_state_broadcaster",
+            "--controller-manager",
+            "/controller_manager",
+        ],
+    )
+
+    kiwi_controller_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["kiwi_controller", "--controller-manager", "/controller_manager"],
+    )
+
+    return LaunchDescription(
+        [
+            control_node,
+            joint_state_broadcaster_spawner,
+            kiwi_controller_spawner,
+            robot_state_publisher_node,
+            rviz_node,
+        ]
+    )
